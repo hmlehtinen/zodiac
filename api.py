2d270b7ed4bf7d5158968458bad6ac43ac4bb2ee
Rob McGinley <mcginleyr1@gmail.com>
2016-04-21 14:17:42 -0400

    Adding functions to facilitate downloading of datasets

--- api.py
+++ api.py
@@ -27,6 +27,9 @@ class Zodiac(object):
         with open(filepath, 'rb') as data:
             resp = self.session.put(url, data=data, headers=headers)
 
+    def _get(self, url):
+        return self.session.get(url)
+
     def login_as(self, username, password):
         self.session = requests.Session()
         url = self._format_url('{api}/{version}/auth/login')
@@ -53,3 +56,25 @@ class Zodiac(object):
             url,
             {'description': description, 'filename': filename, 's3_path': s3path}
         )
+
+
+    def _list_datasets(self):
+        url = self._format_url('{api}/{version}/{company}/datasets/list', company=self.company)
+        return json.loads(self._get(url).text)
+
+    def list_datasets(self):
+        data = self._list_datasets()
+        return [d['filename'] for d in data]
+
+    def get_download_url(self, filename):
+        datasets = self._list_datasets()
+        inst = [d['id'] for d in datasets if d['filename'] == filename][-1]
+        if not inst:
+            raise Exception("Unknown file")
+        resp = self._get(
+            self._format_url(
+                "{api}/{version}/{company}/datasets/{inst}/download_url",
+                company=self.company,
+                inst=inst)
+        )
+        return json.loads(resp.text)['url']
