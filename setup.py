2e476c868f7fceb805120f7ca94684b712b5b180
Rob McGinley <mcginleyr1@gmail.com>
2017-01-31 17:46:25 -0500

    Uprev

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'zodiac',
   packages = ['zodiac'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'CLI Tools to connect to the zodiac api.  May be used as a programatic API but that is not yet documented.',
   author = 'Robert McGinley',
   author_email = 'rob@zodiacmetrics.com',
   url = 'https://github.com/zodiacmetrics/python-client', # use the URL to the github repo
-  download_url = 'https://github.com/zodiacmetrics/python-client/tarball/v0.1', # I'll explain this in a second
+  download_url = 'https://github.com/zodiacmetrics/python-client/tarball/v0.2', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
