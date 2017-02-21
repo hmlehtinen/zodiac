a7643374ab8220f626d0c9af19ee68c1115e6791
craig <craig@zodiacmetrics.com>
2017-02-21 16:45:55 -0500

    add api argument

--- zodiac/zodiac.py
+++ zodiac/zodiac.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--description', dest='description', help='File description', required=True)
     parser.add_argument('-u', '--username', dest='username', help='Who to login as', required=True)
     parser.add_argument('-p', '--password', dest='password', help='Password for user', required=True)
+    parser.add_argument('--api',dest="api",help="The Zodiac API to use",required=False, default=None)
 
     args = parser.parse_args()
-    client = api.Zodiac(args.username, args.password)
+    client = api.Zodiac(args.username, args.password, api=args.api)
     client.upload_file(args.filepath, args.description)
